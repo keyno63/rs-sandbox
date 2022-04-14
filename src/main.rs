651bc9035d50134512e310e1481ba37f5b4a2928
Keyno <kofujiw@gmail.com>
2022-04-15 00:53:44 +0900

    http サーバーの実装

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,38 @@
+extern crate time;
+
+use std::io::Write;
+use std::io::prelude::*;
+use std::net::TcpStream;
+use std::net::TcpListener;
+
 fn main() {
-    println!("Hello, world!");
+    let listener = TcpListener::bind("127.0.0.1:8888").unwrap();
+
+    for stream in listener.incoming() {
+        let stream = stream.unwrap();
+
+        handle_connection(stream);
+    }
+}
+
+fn handle_connection(mut stream: TcpStream) {
+    let mut buffer = [0; 1024];
+
+    stream.read(&mut buffer).unwrap();
+
+    println!("Request: {}", String::from_utf8_lossy(&buffer[..]));
+
+    let message    = "Hello, World!";
+    let time       = time::now_utc();
+    let format     = "%a, %d %b %Y %T GMT";
+    let response   = format!("HTTP/1.1 200 OK\r\n\
+                              Date: {}\r\n\
+                              Content-Type: text/html; charset=utf-8\r\n\
+                              Content-Length: {}\r\n\
+                              \r\n\
+                              {}",
+                             time::strftime(format, &time).unwrap(),
+                             message.len(),
+                             message);
+    let _          = stream.write(response.as_bytes());
 }
