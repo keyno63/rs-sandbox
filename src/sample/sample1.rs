abc3750493702d47cdbfb030d0efb05d8f288966
Keyno <kofujiw@gmail.com>
2022-05-24 00:47:47 +0900

    add test code

--- src/sample/sample1.rs
+++ src/sample/sample1.rs
@@ -1,3 +1,25 @@
+#[derive(Debug, PartialEq)]
 pub struct Sample {
     pub name: String
+}
+
+impl Eq for Sample {}
+
+impl Sample {
+    pub fn new(name: String) -> Self {
+        Self{name}
+    }
+}
+
+#[cfg(test)]
+mod tests {
+    use sample::Sample;
+    use crate::sample;
+
+    #[test]
+    fn test_eq() {
+        let s1 = Sample::new(String::from("name"));
+        let s2 = Sample::new(String::from("name"));
+        assert_eq!(s1, s2);
+    }
 }
